43318b268978b94e83b3587932b383a8ab402fa0
Gabriel Oliveira <gabrieloliveira@semiglobe.com>
2019-09-11 15:32:31 -0300

    This is a commit to test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './css/style.css';
+import './css/mobile.css';
+import './css/ipad.css';
+import mainlogo from './img/logo.png';
+import userAvatar from './img/user.svg';
+import shopCart from './img/shopping-cart.svg';
+import Cards from './modules/Cards.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Navbar extends Component {
+    state = {
+        cardd: () => {
+            console.log(true);
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <nav>
+                    <img src={mainlogo} className="logo" />
+                    <div className="search-bar">
+                        <input type="text" name="search-bar-input" placeholder="Exemplo: saia jeans" />
+                    </div>
+                    <div className="login-section">
+                        <div className="avatar">
+                            <img style={{ display: 'none' }} src={userAvatar} alt="user-icon" />
+                            <div className="custom-avatar"><span>G</span></div>
+                        </div>
+                        <div className="in-out-section">
+                            <div className="login-banner" />
+                            <div className="signup-banner" />
+                        </div>
+                    </div>
+                    <div className="itens-purchased">
+                        <img src={shopCart} alt="shopping-cart" />
+                        <div className="itens-counter-div"><span>5</span></div>
+                    </div>
+                </nav>
+                <button onClick={this.state.cardd}>Teste</button>
+            </div >
+        );
+    }
 }
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar />
+                <div id="teste"></div>
+            </div>
+        );
+    }
+
+};
+
 
 export default App;
