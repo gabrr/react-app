2f6bb4d8a6523b3327179fa90e37228644968fcf
Gabriel Oliveira <gabrieloliveira@semiglobe.com>
2019-09-11 15:53:09 -0300

    adding files

---
+++ src/modules/Cards.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "../css/mobile.css";
+
+class Cards extends Component {
+    render() {
+        return (
+            <h1>Hello world is ever!</h1>
+        );
+    }
+}
+
+export default Cards;
