7ce915085e8b51079de65537bebbfc50528f796f
Gabriel Oliveira <gabrieloliveira@semiglobe.com>
2019-10-16 13:16:05 -0300

    removing side items

--- src/modules/mainframe.jsx
+++ src/modules/mainframe.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import SideMenu from './sideMenu';
 
 class Mainframe extends Component {
     render() {
         return (
             <div id="mainframe">
-                <SideMenu />
+                <p>Hi</p>
             </div>
         );
     }
