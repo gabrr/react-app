90fb19221c4d8fc01787da06bf26ba779a18b561
Gabriel Oliveira <gabrieloliveira@semiglobe.com>
2019-10-16 13:20:46 -0300

    adding navbar component

--- src/modules/mainframe.jsx
+++ src/modules/mainframe.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Navbar from './navbar';
 
 class Mainframe extends Component {
     render() {
         return (
             <div id="mainframe">
-                <p>Hi</p>
+                <Navbar />
             </div>
         );
     }
